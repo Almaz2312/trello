5c9a80bf694b7f097ea2440df9f90c1725be560c
Almaz <almazdurusaliev@gmail.com>
2022-11-24 11:12:02 +0600

    added configuration to CSRF Token

--- main/settings.py
+++ main/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+CSRF_TRUSTED_ORIGINS = [config('CSRF_TRUSTED_ORIGINS')]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
